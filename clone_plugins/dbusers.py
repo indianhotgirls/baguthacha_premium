e5358bb8eb3d17ccdb436a81b8ccdfb5e85ce4ea
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-13 16:31:08 +0530

    Update dbusers.py

--- clone_plugins/dbusers.py
+++ clone_plugins/dbusers.py
@@ -1,4 +1,5 @@
-
+import time
+import datetime
 import motor.motor_asyncio
 from config import CDB_NAME, DB_URI
 
@@ -12,6 +13,7 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.users = self.db.uersz
         self.grp = self.db.groups
 
 
@@ -39,25 +41,48 @@ class Database:
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
-
     
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id':int(id)})
         return bool(user)
 
-
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-
     
     async def get_all_users(self):
         return self.col.find({})
 
-
-
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+    async def get_user(self, user_id):
+        user_data = await self.users.find_one({"id": user_id})
+        return user_data
+            
+    async def update_user(self, user_data):
+        await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
+
+    async def has_premium_access(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            expiry_time = user_data.get("expiry_time")
+            if expiry_time is None:
+                # User previously used the free trial, but it has ended.
+                return False
+            elif isinstance(expiry_time, datetime.datetime) and datetime.datetime.now() <= expiry_time:
+                return True
+            else:
+                await self.users.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
+        return False
+    
+    async def check_remaining_uasge(self, userid):
+        user_id = userid
+        user_data = await self.get_user(user_id)        
+        expiry_time = user_data.get("expiry_time")
+        # Calculate remaining time
+        remaining_time = expiry_time - datetime.datetime.now()
+        return remaining_time
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
