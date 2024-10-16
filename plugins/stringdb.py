a158a98ba23078eb521acf413dfe31c260b06176
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-16 17:40:03 +0530

    Add files via upload

---
+++ plugins/stringdb.py
@@ -0,0 +1,21 @@
+import pymongo
+import random
+import string
+from config import T_DB_URI
+
+# Connect to MongoDB
+client = pymongo.MongoClient(T_DB_URI)
+db = client["string_mapping_db"]
+collection = db["string_mapping"]
+
+def generate_random_string():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(10, 15)))
+
+def save_mapping(random_string, original_string):
+    mapping = {"random_string": random_string, "original_string": original_string}
+    collection.insert_one(mapping)
+
+def get_original_string(random_string):
+    mapping = collection.find_one({"random_string": random_string})
+    return mapping["original_string"] if mapping else None
+
